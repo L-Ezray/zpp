a49ec6805ae89fc42e322497072a9c95f1c8f997
libei <lb456852@outlook.com>
2018-07-11 20:42:37 +0800

    1

---
+++ dibati.py
@@ -0,0 +1,4 @@
+a = input('直角边边长a:')
+b = input('直角边边长b:')
+c = (float(a)*float(a)+float(b)*float(b))**(1/2)
+print('直角三角形的斜边长为:',c)
---
+++ diqiti.py
@@ -0,0 +1,4 @@
+a = input('父亲身高:')
+b = input('母亲身高:')
+c = (float(a)+float(b))*0.54
+print('预测孩子身高为:',c)
